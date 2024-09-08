2ab13f1d30e322599e3f14291d8fd754793babd1
siihasann <mashasanr@gmail.com>
2024-09-08 18:51:31 +0700

    updating welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Role Management Application</title>
+    <title>Role Management Application Hasan</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600,700" rel="stylesheet">
